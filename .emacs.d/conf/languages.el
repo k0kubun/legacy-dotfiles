f9e86ba0b4baea7d2c85d32aaee7c7bd91f3d2b7
ikstrm <igzarion.1993@gmail.com>
2013-01-29 15:40:02 +0900

    disable rcodetools

--- .emacs.d/conf/languages.el
+++ .emacs.d/conf/languages.el
@@ -5,8 +5,8 @@
                       (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
 (require 'ruby-mode)
 (add-to-list 'ruby-encoding-map '(utf-8-hfs . utf-8))
-(require 'rcodetools)
-(define-key ruby-mode-map (kbd "C-c C-d") 'xmp)
+;; (require 'rcodetools)
+;; (define-key ruby-mode-map (kbd "C-c C-d") 'xmp)
 
 ;; PHP
 (when (require 'php-mode nil t)
