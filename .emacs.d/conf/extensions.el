6a3999db9d1f05d8c1eb7426eaf858e7ae23b0de
ikstrm <igzarion.1993@gmail.com>
2013-02-20 02:13:50 +0900

    change yasnippet conf

--- .emacs.d/conf/extensions.el
+++ .emacs.d/conf/extensions.el
@@ -10,5 +10,11 @@
 
 ;; yasnippet
 (when (require 'yasnippet nil t)
-  (yas/initialize)
-  (yas/load-directory "~/.emacs.d/snippets"))
+  (setq yas-snippet-dirs
+         '("~/.emacs.d/snippets")))
+(yas-global-mode 1)
+
+(custom-set-variables '(yas-trigger-key "TAB"))
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
