c345c7af416dacaaa6f30bc94b9396f4cae37028
ikstrm <igzarion.1993@gmail.com>
2013-02-20 02:28:22 +0900

    change flymake conf

--- .emacs.d/conf/extensions.el
+++ .emacs.d/conf/extensions.el
@@ -21,6 +21,19 @@
 
 ;; flymake
 (require 'flymake)
+(set-face-background 'flymake-errline "red4")
+(set-face-background 'flymake-warnline "dark slate blue")
+(defun flymake-ruby-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "ruby" (list "-c" local-file))))
+(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+
 (add-hook
  'ruby-mode-hook
  '(lambda ()
