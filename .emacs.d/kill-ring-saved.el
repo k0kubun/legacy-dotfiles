65722f5e1d0f8861058b507929e5f04fe877a8d5
ikstrm <igzarion.1993@gmail.com>
2013-02-23 18:07:31 +0900

    ignore kill-ring-saved

--- .emacs.d/kill-ring-saved.el
+++
@@ -1,2 +0,0 @@
-(setq kill-ring '(", :platforms => :ruby" "  # See https://github.com/sstephenson/execjs#readme for more supported runtimes" "  gem 'sass-rails',   '~> 3.2.3'" "group :asets do
-g" "dotfiles/.emacs.d/"))
