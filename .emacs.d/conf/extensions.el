6b336742786a922e6cd17a87bb67df4211f80710
ikstrm <igzarion.1993@gmail.com>
2013-02-26 16:50:22 +0900

    configure flymake-able condition

--- .emacs.d/conf/extensions.el
+++ .emacs.d/conf/extensions.el
@@ -35,6 +35,26 @@
 (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
 (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
 
+;; flymake を使えない場合をチェック
+(defadvice flymake-can-syntax-check-file
+  (after my-flymake-can-syntax-check-file activate)
+  (cond
+   ((not ad-return-value))
+   ;; tramp 経由であれば、無効
+   ((and (fboundp 'tramp-list-remote-buffers)
+         (memq (current-buffer) (tramp-list-remote-buffers)))
+    (setq ad-return-value nil))
+   ;; 書き込み不可ならば、flymakeは無効
+   ((not (file-writable-p buffer-file-name))
+    (setq ad-return-value nil))
+   ;; flymake で使われるコマンドが無ければ無効
+   ((let ((cmd (nth 0 (prog1
+                          (funcall (flymake-get-init-function buffer-file-name))
+                        (funcall (flymake-get-cleanup-function buffer-file-name))))))
+      (and cmd (not (executable-find cmd))))
+    (setq ad-return-value nil))
+   ))
+
 (add-hook
  'ruby-mode-hook
  '(lambda ()
