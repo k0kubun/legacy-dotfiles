edaa483cabb90af0e68cbdc316ce31bf4faa55d8
ikstrm <igzarion.1993@gmail.com>
2013-02-26 18:03:26 +0900

    sad to disable flymake

--- .emacs.d/conf/extensions.el
+++ .emacs.d/conf/extensions.el
@@ -21,45 +21,25 @@
 (define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
 
 ;; flymake
-(require 'flymake)
-(set-face-background 'flymake-errline "red4")
-(set-face-background 'flymake-warnline "dark slate blue")
-(defun flymake-ruby-init ()
-  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-         (local-file  (file-relative-name
-                       temp-file
-                       (file-name-directory buffer-file-name))))
-    (list "ruby" (list "-c" local-file))))
-(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+;; (require 'flymake)
+;; (set-face-background 'flymake-errline "red4")
+;; (set-face-background 'flymake-warnline "dark slate blue")
+;; (defun flymake-ruby-init ()
+;;   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;;          (local-file  (file-relative-name
+;;                        temp-file
+;;                        (file-name-directory buffer-file-name))))
+;;     (list "ruby" (list "-c" local-file))))
+;; (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+;; (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+;; (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
 
-;; flymake を使えない場合をチェック
-(defadvice flymake-can-syntax-check-file
-  (after my-flymake-can-syntax-check-file activate)
-  (cond
-   ((not ad-return-value))
-   ;; tramp 経由であれば、無効
-   ((and (fboundp 'tramp-list-remote-buffers)
-         (memq (current-buffer) (tramp-list-remote-buffers)))
-    (setq ad-return-value nil))
-   ;; 書き込み不可ならば、flymakeは無効
-   ((not (file-writable-p buffer-file-name))
-    (setq ad-return-value nil))
-   ;; flymake で使われるコマンドが無ければ無効
-   ((let ((cmd (nth 0 (prog1
-                          (funcall (flymake-get-init-function buffer-file-name))
-                        (funcall (flymake-get-cleanup-function buffer-file-name))))))
-      (and cmd (not (executable-find cmd))))
-    (setq ad-return-value nil))
-   ))
-
-(add-hook
- 'ruby-mode-hook
- '(lambda ()
-    ;; Don't want flymake mode for ruby regions in rhtml files
-    (if (not (null buffer-file-name)) (flymake-mode))))
+;; (add-hook
+;;  'ruby-mode-hook
+;;  '(lambda ()
+;;     ;; Don't want flymake mode for ruby regions in rhtml files
+;;     (if (not (null buffer-file-name)) (flymake-mode))))
 
 ;; elscreen
 (load "elscreen" "ElScreen" t)
