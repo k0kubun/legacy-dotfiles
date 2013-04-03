389a18610da1909b804e32ca381f2e73c74dc2d1
ikstrm <mail@ikst.jp>
2013-04-03 14:36:35 +0900

    Add Objective-C configuration

--- .emacs.d/conf/languages.el
+++ .emacs.d/conf/languages.el
@@ -50,9 +50,39 @@
   (setq php-search-url "http://jp.php.net/ja/")
   (setq php-manual-url "http://jp.php.net/manual/ja"))
 
+;; Objective-C
+(add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*\n@implementation" . objc-mode))
+(add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*\n@interface" . objc-mode))
+(add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*\n@protocol" . objc-mode))
+
+(ffap-bindings)
+(setq ffap-newfile-prompt t)
+(setq ffap-kpathsea-depth 5)
+
+(setq ff-other-file-alist
+     '(("\\.mm?$" (".h"))
+       ("\\.cc$"  (".hh" ".h"))
+       ("\\.hh$"  (".cc" ".C"))
+
+       ("\\.c$"   (".h"))
+       ("\\.h$"   (".c" ".cc" ".C" ".CC" ".cxx" ".cpp" ".m" ".mm"))
+
+       ("\\.C$"   (".H"  ".hh" ".h"))
+       ("\\.H$"   (".C"  ".CC"))
+
+       ("\\.CC$"  (".HH" ".H"  ".hh" ".h"))
+       ("\\.HH$"  (".CC"))
+
+       ("\\.cxx$" (".hh" ".h"))
+       ("\\.cpp$" (".hpp" ".hh" ".h"))
+
+       ("\\.hpp$" (".cpp" ".c"))))
+(add-hook 'objc-mode-hook
+         (lambda ()
+           (define-key c-mode-base-map (kbd "C-c o") 'ff-find-other-file)
+         ))
+
 ;; C++
-(setq auto-mode-alist
-      (cons (cons "\\.mm$" 'c++-mode) auto-mode-alist))
 (setq auto-mode-alist
       (cons (cons "\\.h$" 'c++-mode) auto-mode-alist))
 (require 'google-c-style)
