a685927b29b2844bb20ac1450047177cc7cabbcd
ikstrm <mail@ikst.jp>
2013-04-03 14:43:19 +0900

    Fix other-file-alist for obj-C

--- .emacs.d/conf/languages.el
+++ .emacs.d/conf/languages.el
@@ -54,29 +54,14 @@
 (add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*\n@implementation" . objc-mode))
 (add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*\n@interface" . objc-mode))
 (add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*\n@protocol" . objc-mode))
-
+ 
 (ffap-bindings)
 (setq ffap-newfile-prompt t)
 (setq ffap-kpathsea-depth 5)
 
 (setq ff-other-file-alist
      '(("\\.mm?$" (".h"))
-       ("\\.cc$"  (".hh" ".h"))
-       ("\\.hh$"  (".cc" ".C"))
-
-       ("\\.c$"   (".h"))
-       ("\\.h$"   (".c" ".cc" ".C" ".CC" ".cxx" ".cpp" ".m" ".mm"))
-
-       ("\\.C$"   (".H"  ".hh" ".h"))
-       ("\\.H$"   (".C"  ".CC"))
-
-       ("\\.CC$"  (".HH" ".H"  ".hh" ".h"))
-       ("\\.HH$"  (".CC"))
-
-       ("\\.cxx$" (".hh" ".h"))
-       ("\\.cpp$" (".hpp" ".hh" ".h"))
-
-       ("\\.hpp$" (".cpp" ".c"))))
+       ("\\.cc$"  (".hh" ".h"))))
 (add-hook 'objc-mode-hook
          (lambda ()
            (define-key c-mode-base-map (kbd "C-c o") 'ff-find-other-file)
