15130de9a83d3cf1f37878107c0a164ac16be3c2
ikstrm <mail@ikst.jp>
2013-04-14 14:08:32 +0900

    Add setup shell script and README

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+cd ~/source  
+git clone https://github.com/ikstrm/dotfiles  
+cd dotfiles  
+sh setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,12 @@
+ln -s ~/source/dotfiles/.emacs.d ~/.emacs.d
+ln -s ~/source/dotfiles/.vim ~/.vim
+ln -s ~/source/dotfiles/.vimrc ~/.vimrc
+ln -s ~/source/dotfiles/.gvimrc ~/.gvimrc
+ln -s ~/source/dotfiles/.zshrc ~/.zshrc
+ln -s ~/source/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/source/dotfiles/.gitconfig ~/.gitconfig
+rm ~/Library/Application\ Support/KeyRemap4MacBook/private.xml 
+ln -s ~/source/dotfiles/etc/private.xml ~/Library/Application\ Support/KeyRemap4MacBook/private.xml 
+
+
+
