1d3d2dee542b9944c9d58f4b5e58b34a0f45d624
ikstrm <mail@ikst.jp>
2013-05-04 20:25:37 +0900

    Remove trailing whitespace in c++-mode

--- .emacs.d/conf/languages.el
+++ .emacs.d/conf/languages.el
@@ -24,6 +24,11 @@
 (require 'google-c-style)
 (add-hook 'c-mode-common-hook 'google-make-newline-indent)
 (add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c++-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook 'delete-trailing-whitespace)
+            ))
+
 
 ;; javascript
 (require 'js)
