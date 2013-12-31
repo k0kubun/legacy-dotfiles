cf3efaa7f9ed9d054608774576e7d9551cf1df95
Takashi Kokubun <takashikkbn@gmail.com>
2013-12-31 17:44:57 +0900

    Update install script

--- setup.sh
+++
@@ -1,7 +0,0 @@
-ln -s `pwd`/.vim ~/.vim
-ln -s `pwd`/.vimrc ~/.vimrc
-ln -s `pwd`/.zshrc ~/.zshrc
-ln -s `pwd`/.tmux.conf ~/.tmux.conf
-ln -s `pwd`/.gitconfig ~/.gitconfig
-rm ~/Library/Application\ Support/KeyRemap4MacBook/private.xml
-ln -s `pwd`/etc/private.xml ~/Library/Application\ Support/KeyRemap4MacBook/private.xml
