da46cdcd96fbd14816fea79aaebe662adc36a49d
Takashi Kokubun <takashikkbn@gmail.com>
2014-06-09 12:33:23 +0900

    Add GOMAXPROCS

--- .zsh/lib/languages.zsh
+++ .zsh/lib/languages.zsh
@@ -19,5 +19,6 @@ function ttp() {
 export GOPATH=${HOME}/go
 export PATH="${GOPATH}/bin:$PATH"
 export GOROOT="/usr/local/opt/go/libexec"
+export GOMAXPROCS=4
 alias g="/usr/local/bin/go"
 alias gor="/usr/local/bin/go run"
