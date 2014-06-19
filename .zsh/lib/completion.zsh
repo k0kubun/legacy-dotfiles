2889ca445340a242afc680c219e06b7a383c7742
Takashi Kokubun <takashi-kokubun@cookpad.com>
2014-06-19 10:56:32 +0900

    Fix some completion

--- .zsh/lib/completion.zsh
+++ .zsh/lib/completion.zsh
@@ -1,6 +1,6 @@
 # use completion
-# autoload -U compinit
-# compinit
+autoload -U compinit
+compinit
 
 # select completion by cursor
 # zstyle ':completion:*:default' menu select=2
@@ -9,17 +9,17 @@
 # setopt magic_equal_subst
 
 # complete / of dir name
-# setopt auto_param_slash
-# setopt mark_dirs
+setopt auto_param_slash
+setopt mark_dirs
 
 # colorize completions
-zstyle ':completion:*' verbose yes
-zstyle ':completion:*' completer _expand _complete _match _prefix _approximate _list _history
-zstyle ':completion:*:messages' format '%F{YELLOW}%d'$DEFAULT
-zstyle ':completion:*:warnings' format '%F{RED}No matches for:''%F{YELLOW} %d'$DEFAULT
-zstyle ':completion:*:descriptions' format '%F{YELLOW}completing %B%d%b'$DEFAULT
-zstyle ':completion:*:options' description 'yes'
-zstyle ':completion:*:descriptions' format '%F{yellow}Completing %B%d%b%f'$DEFAULT
+# zstyle ':completion:*' verbose yes
+# zstyle ':completion:*' completer _expand _complete _match _prefix _approximate _list _history
+# zstyle ':completion:*:messages' format '%F{YELLOW}%d'$DEFAULT
+# zstyle ':completion:*:warnings' format '%F{RED}No matches for:''%F{YELLOW} %d'$DEFAULT
+# zstyle ':completion:*:descriptions' format '%F{YELLOW}completing %B%d%b'$DEFAULT
+# zstyle ':completion:*:options' description 'yes'
+# zstyle ':completion:*:descriptions' format '%F{yellow}Completing %B%d%b%f'$DEFAULT
 
 # group completions
 # zstyle ':completion:*' group-name ''
