de6af33c404b202c83c8f43539e1367489f9a05a
Takashi Kokubun <takashikkbn@gmail.com>
2014-06-20 17:15:55 +0900

    Change GOPATH

--- .zsh/lib/go.zsh
+++ .zsh/lib/go.zsh
@@ -1,7 +1,6 @@
 # Go
-export GOPATH=${HOME}/go
+export GOPATH=$HOME
 export PATH="${GOPATH}/bin:$PATH"
 export GOROOT="/usr/local/opt/go/libexec"
 export GOMAXPROCS=4
 alias g="/usr/local/bin/go"
-alias gor="/usr/local/bin/go run"
