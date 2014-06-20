96991d4c8d7f991b1b3abe756e3e39cd8ce6c816
Takashi Kokubun <takashikkbn@gmail.com>
2014-06-20 17:12:54 +0900

    Split zsh configuration for go into go.zsh

---
+++ .zsh/lib/go.zsh
@@ -0,0 +1,7 @@
+# Go
+export GOPATH=${HOME}/go
+export PATH="${GOPATH}/bin:$PATH"
+export GOROOT="/usr/local/opt/go/libexec"
+export GOMAXPROCS=4
+alias g="/usr/local/bin/go"
+alias gor="/usr/local/bin/go run"
