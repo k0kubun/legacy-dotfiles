d1bfdad5f8234c3d5f8b0caccd89547cb62ce688
Takashi Kokubun <takashikkbn@gmail.com>
2014-07-14 18:20:48 +0900

    Autobuild dotremap

---
+++ .zsh/lib/daemons.zsh
@@ -0,0 +1,25 @@
+if ! which dotremap > /dev/null; then
+  echo "Installing dotremap..."
+  sudo gem install dotremap
+
+  if ! which dotremap > /dev/null; then
+    echo "failed to install dotremap"
+    return
+  fi
+fi
+
+
+if ! which autobuild > /dev/null; then
+  echo "Installing autobuild..."
+  go get -u github.com/k0kubun/autobuild
+
+  if ! which autobuild > /dev/null; then
+    echo "failed to install autobuild"
+    return
+  fi
+fi
+
+if ! pgrep -f autobuild_dotremap > /dev/null; then
+  echo "Launched dotremap autobuild daemon"
+  nohup autobuild "dotremap" -f ~/.remap -i autobuild_dotremap > /dev/null &
+fi
