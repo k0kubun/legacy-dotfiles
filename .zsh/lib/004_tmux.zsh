1410a8770dfd3161e25c10746bb5f557f6412c0a
Takashi Kokubun <takashikkbn@gmail.com>
2014-07-28 15:03:45 +0900

    Make tmux initializer simpler

--- .zsh/lib/004_tmux.zsh
+++ .zsh/lib/004_tmux.zsh
@@ -2,16 +2,8 @@
 export PATH="$HOME/bin:$PATH"
 alias tmux="tmux -2"
 if [ -z "$TMUX" -a -z "$STY" ]; then
-	if type tmuxx >/dev/null 2>&1; then
-		tmuxx
-	elif type tmux >/dev/null 2>&1; then
-		if tmux has-session && tmux list-sessions | /usr/bin/grep -qE '.*]$'; then
-			tmux attach && echo "tmux attached session "
-		else
-			tmux new-session && echo "tmux created new session"
-		fi
-	elif type screen >/dev/null 2>&1; then
-		screen -rx || screen -D -RR
+	if type tmux > /dev/null 2>&1; then
+		tmux new-session -A -s "scratch"
 	fi
 fi
 
