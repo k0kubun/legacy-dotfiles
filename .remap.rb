4d8a6323b787f54c58688218e2f244bcb693fdd7
Takashi Kokubun <takashikkbn@gmail.com>
2014-07-28 20:52:36 +0900

    C-pnbf binding for not terminal

--- .remap.rb
+++ .remap.rb
@@ -61,7 +61,7 @@ item "CapsLock Mode" do
   show_message "CapsLock"
 end
 
-item "Control+PNBF to Up/Down/Left/Right" do
+item "Control+PNBF to Up/Down/Left/Right", not: "TERMINAL" do
   remap "C-p", to: "Up"
   remap "C-n", to: "Down"
   remap "C-b", to: "Left"
