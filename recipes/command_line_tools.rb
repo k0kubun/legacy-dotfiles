66e79b11036180578c28d5c0e0623bf9c735e776
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-20 00:00:47 +0900

    Fix command line tools script

--- recipes/command_line_tools.rb
+++ recipes/command_line_tools.rb
@@ -1,11 +1,9 @@
-INSTALL_CHECK   = "xcode-select -p"
-
 execute "xcode-select --install" do
-  not_if INSTALL_CHECK
+  not_if "xcode-select -p"
 end
-execute %Q{ruby -e 'sleep 5 until system("#{INSTALL_CHECK}")'}
+execute %Q{ruby -e 'sleep 5 until system("xcode-select -p")'}
 
 agree_script = File.expand_path("../../bin/agree_license", __FILE__)
-execute agree_script do
+execute "sudo #{agree_script}" do
   only_if "gcc 2>&1 | grep Agreeing"
 end
