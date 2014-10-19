5a3eeff0a0cc9a4b20878e06ffcf0ef5e5891152
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-19 13:57:56 +0900

    Create primary recipes structure

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,1 +1,15 @@
-puts "Hello world!"
+# These recipes are executed first in this order
+PRIMARY_RECIPES = %w[
+  git
+]
+
+recipes_dir = File.expand_path("../recipes", __FILE__)
+
+all_recipes = Dir.glob(File.join(recipes_dir, "*"))
+primary_recipes = PRIMARY_RECIPES.map do |recipe_name|
+  File.join(recipes_dir, "#{recipe_name}.rb")
+end
+
+(primary_recipes + (all_recipes - primary_recipes)).each do |recipe|
+  include_recipe recipe
+end
---
+++ recipes/git.rb
@@ -0,0 +1,1 @@
+puts "Install git"
