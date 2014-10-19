8b8f191bcb3ec85c32d4c232ae5211104258c149
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-19 13:09:42 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,20 +6,21 @@ branch=master
 
 tempdir=/tmp
 archive=${tempdir}/${repo}.zip
-unzipped=${tempdir}/${repo}-${branch}
+workdir=${tempdir}/${repo}-${branch}
 
-# Fetch repository to /tmp/dotfiles-master
-echo "Fetching dotfiles repository..."
-curl -L -o $archive https://github.com/${user}/${repo}/archive/${branch}.zip 2> /dev/null
-rm -rf $unzipped
-unzip -o $archive -d $tempdir > /dev/null
+if [ ! -e $workdir ]; then
+  # Fetch repository to /tmp/dotfiles-master
+  echo -n "Fetching dotfiles repository..."
+  curl -L -o $archive https://github.com/${user}/${repo}/archive/${branch}.zip 2> /dev/null
+  unzip -o $archive -d $tempdir > /dev/null
+  rm $archive
+  echo " done."
+fi
 
-# Run installer
-echo "Running installer..."
-pushd $unzipped > /dev/null
-# WIP
+# Update repository
+echo "Updating repository..."
+pushd $workdir > /dev/null
 
 # Cleanup
 popd > /dev/null
-rm $archive
 echo "Finished to install dotfiles."
