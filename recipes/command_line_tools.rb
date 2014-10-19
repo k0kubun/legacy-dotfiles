b73c12c29c7bf672b10dc9f05aedf4101dbc910f
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-19 23:47:02 +0900

    Automatic agreement

--- recipes/command_line_tools.rb
+++ recipes/command_line_tools.rb
@@ -1,3 +1,11 @@
+INSTALL_CHECK   = "xcode-select -p"
+
 execute "xcode-select --install" do
-  not_if "git version 2>&1 | grep '^git version'"
+  not_if INSTALL_CHECK
+end
+execute %Q{ruby -e 'sleep 5 until system("#{INSTALL_CHECK}")'}
+
+agree_script = File.expand_path("../../bin/agree_license", __FILE__)
+execute agree_script do
+  only_if "gcc 2>&1 | grep Agreeing"
 end
