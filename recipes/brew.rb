1e23929206060b5a523aa77c6a97d22adc5e4b22
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-21 22:19:16 +0900

    Install many brew packages

--- recipes/brew.rb
+++ recipes/brew.rb
@@ -1,10 +1,22 @@
 BREW_INSTALLER = "https://raw.githubusercontent.com/Homebrew/install/master/install"
 
+INSTALL_PACKAGES = %[
+  go
+  tmux
+  mysql
+  rbenv
+  ruby-build
+  reattach-to-user-namespace
+]
+
 execute %Q[ruby -e "$(curl -fsSL #{BREW_INSTALLER})"] do
   not_if "which brew"
 end
 
-package "tmux"
+INSTALL_PACKAGES.each do |pkg|
+  package pkg
+end
+
 package "vim" do
   options "--with-lua --with-luajit"
 end
