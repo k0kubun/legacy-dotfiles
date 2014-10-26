66d6100d28a1ad32f228aa496574332e9977de81
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-26 19:19:37 +0900

    Use dependency resolver

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 repo_path=~/src/github.com/k0kubun/dotfiles
-rebuild k0kubun/dotfiles -s script -d $repo_path
+
+rebuild k0kubun/dotfiles -s script -d $repo_path <<-PRIMARY
+  brew.sh
+PRIMARY
