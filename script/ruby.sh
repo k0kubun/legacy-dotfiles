00c0b775056935a879ade32ddc5fa7ec3dcf9dd4
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-26 22:35:48 +0900

    Fix ruby

--- script/ruby.sh
+++ script/ruby.sh
@@ -5,16 +5,17 @@ INSTALL_OPTIONS="
   --with-readline-dir=$(brew --prefix readline)
   --with-openssl-dir=$(brew --prefix openssl)
 "
-# DEFAULT_GEMS="
-#   bundler
-#   rebuild
-# "
+DEFAULT_GEMS="
+  bundler
+  rebuild
+"
 
 if rbenv versions | grep $VERSION > /dev/null; then
   echo "Skip: rbenv install ${VERSION}"
 else
   RUBY_CONFIGURE_OPTS="${INSTALL_OPTIONS}" rbenv install ${VERSION}
-  # rbenv global $VERSION
+  rbenv global $VERSION
+  rbenv rehash
 fi
 
 # for gem_name in $DEFAULT_GEMS; do
