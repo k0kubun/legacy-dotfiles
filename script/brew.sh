f999b7bbad7365507417c9c12705ed1c0d11588e
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-26 16:20:06 +0900

    Install homebrew

--- script/brew.sh
+++ script/brew.sh
@@ -4,3 +4,24 @@ if ! which brew > /dev/null; then
   install_script_url=https://raw.githubusercontent.com/Homebrew/install/master/install
   ruby -e "$(curl -fsSL $install_script_url)"
 fi
+
+PACKAGES="
+  go
+  tmux
+  rbenv
+  ruby-build
+  reattach-to-user-namespace
+
+  peco/peco/peco
+  caskroom/cask/brew-cask
+"
+
+for package in $PACKAGES; do
+  package_name=$(basename ${package})
+
+  if brew list -1 | grep "^${package_name}" > /dev/null; then
+    echo "Skip: brew install ${package}"
+  else
+    brew install $package
+  fi
+done
