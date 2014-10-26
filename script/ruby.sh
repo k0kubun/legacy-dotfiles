65f666c3941324dad4c2cb416ef836f6410d6b64
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-26 19:32:50 +0900

    Fix ruby.sh

--- script/ruby.sh
+++ script/ruby.sh
@@ -2,12 +2,11 @@
 
 version="2.1.2"
 
+install_options="
+  --with-readline-dir=$(brew --prefix readline)
+  --with-openssl-dir=$(brew --prefix openssl)
+"
+
 if rbenv versions | grep -v $version > /dev/null; then
-  sh <<-EOS
-    RUBY_CONFIGURE_OPTS="
-      --with-readline-dir=$(brew --prefix readline)
-      --with-openssl-dir=$(brew --prefix openssl)
-    "
-    rbenv install ${version}
-  EOS
+  RUBY_CONFIGURE_OPTS="${install_options}" rbenv install ${version}
 fi
