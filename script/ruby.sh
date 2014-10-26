7f4440135978d39eb90eb360b2e29143cfc403af
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-26 19:49:14 +0900

    Add default gems

--- script/ruby.sh
+++ script/ruby.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
-version="2.1.2"
-
-install_options="
+VERSION="2.1.2"
+INSTALL_OPTIONS="
   --with-readline-dir=$(brew --prefix readline)
   --with-openssl-dir=$(brew --prefix openssl)
 "
+DEFAULT_GEMS="
+  bundler
+  rebuild
+"
 
-if rbenv versions | grep -v $version > /dev/null; then
-  RUBY_CONFIGURE_OPTS="${install_options}" rbenv install ${version}
+if rbenv versions | grep $VERSION > /dev/null; then
+  echo "Skip: rbenv install ${VERSION}"
+else
+  RUBY_CONFIGURE_OPTS="${INSTALL_OPTIONS}" rbenv install ${VERSION}
+  rbenv "$(rbenv init -)"
+  rbenv global $VERSION
 fi
+
+for gem_name in $DEFAULT_GEMS; do
+  if gem list | grep $gem_name > /dev/null; then
+    echo "Skip: gem install ${gem_name}"
+  else
+    gem install $gem_name
+  fi
+done
