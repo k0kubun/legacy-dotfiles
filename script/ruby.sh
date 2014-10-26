9d64cf5f4cc221f1b60ab8e23f586590c7a97fcf
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-26 22:35:16 +0900

    Comment out gem install

--- script/ruby.sh
+++ script/ruby.sh
@@ -5,23 +5,22 @@ INSTALL_OPTIONS="
   --with-readline-dir=$(brew --prefix readline)
   --with-openssl-dir=$(brew --prefix openssl)
 "
-DEFAULT_GEMS="
-  bundler
-  rebuild
-"
+# DEFAULT_GEMS="
+#   bundler
+#   rebuild
+# "
 
 if rbenv versions | grep $VERSION > /dev/null; then
   echo "Skip: rbenv install ${VERSION}"
 else
   RUBY_CONFIGURE_OPTS="${INSTALL_OPTIONS}" rbenv install ${VERSION}
-  rbenv "$(rbenv init -)"
-  rbenv global $VERSION
+  # rbenv global $VERSION
 fi
 
-for gem_name in $DEFAULT_GEMS; do
-  if gem list | grep $gem_name > /dev/null; then
-    echo "Skip: gem install ${gem_name}"
-  else
-    gem install $gem_name
-  fi
-done
+# for gem_name in $DEFAULT_GEMS; do
+#   if gem list | grep $gem_name > /dev/null; then
+#     echo "Skip: gem install ${gem_name}"
+#   else
+#     gem install $gem_name
+#   fi
+# done
