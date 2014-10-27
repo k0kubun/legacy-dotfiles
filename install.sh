0da03a2e2367ae7c14eeb26b2ca3b38d8bad281d
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-27 10:45:41 +0900

    Update rebuild config

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 repo_path=~/src/github.com/k0kubun/dotfiles
 
-rebuild k0kubun/dotfiles -s script -d $repo_path <<-PRIMARY
+rebuild k0kubun/dotfiles -f -s script -d $repo_path <<-PRIMARY
   brew.sh
 PRIMARY
