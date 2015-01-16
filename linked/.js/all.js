8c159dedaa9a12070f3b81ea1b4b79764947e8ff
Takashi Kokubun <takashikkbn@gmail.com>
2015-01-17 02:46:16 +0900

    Add some user scripts

--- linked/.js/all.js
+++ linked/.js/all.js
@@ -1,7 +1,11 @@
 $(window).load(function() {
-  if (location.href.match(/^https?:\/\/github.com/)) {
-    console.log('github');
-  } else if (location.href.match(/^https?:\/\/www.nicovideo.jp/)) {
+  if (location.href.match(/^https?:\/\/(www.|)google.co.jp/)) {
+    // Force to disable personalized search
+    if ($('form[action="/search"]').length > 0) {
+      $('form[action="/search"]').append('<input type="hidden" name="pws" value="0">')
+    }
+  } else if (location.href.match(/^https?:\/\/(www.|)nicovideo.jp/)) {
+    // Replace unified ranking with movie ranking
     if ($('a[href="uni_ranking"]').length > 0) {
       $('a[href="uni_ranking"] span').text('ランキング');
       $('a[href="uni_ranking"]').attr('href', 'ranking');
--- linked/.js/nicovideo.jp.js
+++ linked/.js/nicovideo.jp.js
@@ -1,3 +1,4 @@
+// Replace unified ranking with movie ranking
 if ($('a[href="uni_ranking"]').length > 0) {
   $('a[href="uni_ranking"] span').text('ランキング');
   $('a[href="uni_ranking"]').attr('href', 'ranking');
