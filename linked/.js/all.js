2990706757965ef950c9ac9fd9dc3a63f13d2ada
Takashi Kokubun <takashikkbn@gmail.com>
2015-01-25 00:34:34 +0900

    I stopped using tag feed

--- linked/.js/all.js
+++ linked/.js/all.js
@@ -9,34 +9,4 @@ if (location.href.match(/^https?:\/\/(www.|)google.co.jp/)) {
     $('a[href="uni_ranking"] span').text('ランキング');
     $('a[href="uni_ranking"]').attr('href', 'ranking');
   }
-} else if (location.href.match(/^https?:\/\/(www.|)qiita.com/)) {
-  if (location.pathname != '/') {
-    return;
-  }
-
-  var loadedCount = 0;
-  var ignoreExpression = /( \| |#ruby)/;
-
-  function filterTracks() {
-    $('.item-box.track').each(function() {
-      var text = $(this).find('h1 a').text();
-      if (text.match(ignoreExpression)) {
-        $(this).hide();
-      }
-    });
-  }
-
-  function tryFilter() {
-    if ($('.item-box.track').length > loadedCount) {
-      filterTracks();
-      loadedCount = $('.item-box.track').length;
-    } else {
-      setTimeout(tryFilter, 100);
-    }
-  }
-
-  setTimeout(tryFilter, 100);
-  $(document).delegate('.more-button', 'click', function(event) {
-    setTimeout(tryFilter, 100);
-  });
 }
