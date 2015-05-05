ecffc33b284f56408044c7a4b7ce98d1219c241c
Takashi Kokubun <takashikkbn@gmail.com>
2015-05-06 01:25:42 +0900

    Delete all

--- linked/.js/nicovideo.jp.js
+++
@@ -1,5 +0,0 @@
-// Replace unified ranking with movie ranking
-if ($('a[href="uni_ranking"]').length > 0) {
-  $('a[href="uni_ranking"] span').text('ランキング');
-  $('a[href="uni_ranking"]').attr('href', 'ranking');
-}
--- linked/.vimrc
+++
@@ -1,8 +0,0 @@
-source ~/.vim/bundle.vim
-
-source ~/.vim/basic.vim
-source ~/.vim/indentation.vim
-
-source ~/.vim/skin.vim
-source ~/.vim/plugin.vim
-source ~/.vim/bind.vim
