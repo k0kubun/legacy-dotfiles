719254ab2ab6b67564a80b2e5c589ef564c566b3
Takashi Kokubun <takashikkbn@gmail.com>
2015-05-06 03:24:55 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+user=k0kubun
+repo=dotfiles
+workdir=~/src/github.com/${user}/${repo}
+
+# Clone repository
+mkdir -p ~/src/github.com/${user}
+git clone https://github.com/${user}/${repo} $workdir
+
+# Bundle install
+sudo which bundle > /dev/null || sudo gem install bundler
+pushd $workdir && sudo bundle install > /dev/null
+
+# Run serverkit
+sudo bundle exec serverkit apply recipe.yml
+popd $workdir
