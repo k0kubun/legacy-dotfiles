75523aa4e0cbc81c222940d335b75f1201aa7ab4
Takashi Kokubun <takashikkbn@gmail.com>
2015-05-06 15:44:36 +0900

    Run old scripts

---
+++ scripts/font.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git clone https://gist.github.com/1838072.git /tmp/monaco-for-powerline
+mv /tmp/monaco-for-powerline/Monaco\ for\ Powerline.otf ~/Library/Fonts
+rm -rf /tmp/monaco-for-powerline
