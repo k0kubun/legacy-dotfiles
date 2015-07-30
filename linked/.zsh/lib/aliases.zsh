113c866e9a4da03d41ea117e6991b2d23b718bfb
Takashi Kokubun <takashikkbn@gmail.com>
2015-07-30 22:47:12 +0900

    Open a file with a line using substitution

--- linked/.zsh/lib/aliases.zsh
+++ linked/.zsh/lib/aliases.zsh
@@ -8,7 +8,9 @@ alias ls="ls -Gla"
 alias re="source ~/.zshrc"
 
 # vim
-alias vi="/usr/local/bin/vim"
+function vi() {
+	/usr/local/bin/vim ${=*/:/ +}
+}
 alias vim="/usr/local/bin/vim"
 
 # cd by dir name only for ..
