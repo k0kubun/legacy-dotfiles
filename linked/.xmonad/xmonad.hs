9007eb79be01171745a112f0fe60bc1afc7f2583
Takashi Kokubun <takashikkbn@gmail.com>
2015-08-22 18:32:20 +0900

    Use urxvt by default

--- linked/.xmonad/xmonad.hs
+++ linked/.xmonad/xmonad.hs
@@ -1,4 +1,11 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
 main = do
   xmonad $ defaultConfig
+    { terminal = "urxvt"
+    }
