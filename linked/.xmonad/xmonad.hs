6688109274300bbdb55db0d4c603851f56125d56
Takashi Kokubun <takashikkbn@gmail.com>
2015-09-13 21:04:53 +0900

    Pull default binding

--- linked/.xmonad/xmonad.hs
+++ linked/.xmonad/xmonad.hs
@@ -25,40 +25,60 @@ defaults = defaultConfig
 
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-  [ ((modm, xK_Return), spawn $ XMonad.terminal conf)
-  , ((modm, xK_p), spawn "dmenu_run")
-  -- close focused window
-  , ((modm .|. shiftMask, xK_c), kill)
-   -- Rotate through the available layout algorithms
-  , ((modm, xK_space ), sendMessage NextLayout)
-  --  Reset the layouts on the current workspace to default
-  , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
-  -- Resize viewed windows to the correct size
-  , ((modm, xK_n), refresh)
-  -- Move focus to the next window
-  , ((modm, xK_Tab), windows W.focusDown)
-  -- Move focus to the next window
-  , ((modm, xK_j), windows W.focusDown)
-  -- Move focus to the previous window
-  , ((modm, xK_k), windows W.focusUp  )
-  -- Swap the focused window and the master window
-  , ((modm .|. shiftMask, xK_Return), windows W.swapMaster)
-  -- Swap the focused window with the next window
-  , ((modm .|. shiftMask, xK_j), windows W.swapDown  )
-  -- Swap the focused window with the previous window
-  , ((modm .|. shiftMask, xK_k), windows W.swapUp    )
-  -- Shrink the master area
-  , ((modm, xK_h), sendMessage Shrink)
-  -- Expand the master area
-  , ((modm, xK_l), sendMessage Expand)
-  -- Push window back into tiling
-  , ((modm, xK_t), withFocused $ windows . W.sink)
-  , ((modm .|. shiftMask, xK_t), withFocused $ \w -> floatLocation w >>= windows . W.float w . snd)
-  -- Increment the number of windows in the master area
-  , ((modm, xK_comma ), sendMessage (IncMasterN 1))
-  -- Deincrement the number of windows in the master area
-  , ((modm, xK_period), sendMessage (IncMasterN (-1)))
+  -- launching and killing programs
+  [ ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf) -- %! Launch terminal
+  , ((modMask,               xK_p     ), spawn "dmenu_run") -- %! Launch dmenu
+  , ((modMask .|. shiftMask, xK_p     ), spawn "gmrun") -- %! Launch gmrun
+  , ((modMask .|. shiftMask, xK_c     ), kill) -- %! Close the focused window
+
+  , ((modMask,               xK_space ), sendMessage NextLayout) -- %! Rotate through the available layout algorithms
+  , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf) -- %!  Reset the layouts on the current workspace to default
+
+  , ((modMask,               xK_n     ), refresh) -- %! Resize viewed windows to the correct size
+
+  -- move focus up or down the window stack
+  , ((modMask,               xK_Tab   ), windows W.focusDown) -- %! Move focus to the next window
+  , ((modMask .|. shiftMask, xK_Tab   ), windows W.focusUp  ) -- %! Move focus to the previous window
+  , ((modMask,               xK_j     ), windows W.focusDown) -- %! Move focus to the next window
+  , ((modMask,               xK_k     ), windows W.focusUp  ) -- %! Move focus to the previous window
+  , ((modMask,               xK_m     ), windows W.focusMaster  ) -- %! Move focus to the master window
+
+  -- modifying the window order
+  , ((modMask,               xK_Return), windows W.swapMaster) -- %! Swap the focused window and the master window
+  , ((modMask .|. shiftMask, xK_j     ), windows W.swapDown  ) -- %! Swap the focused window with the next window
+  , ((modMask .|. shiftMask, xK_k     ), windows W.swapUp    ) -- %! Swap the focused window with the previous window
+
+  -- resizing the master/slave ratio
+  , ((modMask,               xK_h     ), sendMessage Shrink) -- %! Shrink the master area
+  , ((modMask,               xK_l     ), sendMessage Expand) -- %! Expand the master area
+
+  -- floating layer support
+  , ((modMask,               xK_t     ), withFocused $ windows . W.sink) -- %! Push window back into tiling
+
+  -- increase or decrease number of windows in the master area
+  , ((modMask              , xK_comma ), sendMessage (IncMasterN 1)) -- %! Increment the number of windows in the master area
+  , ((modMask              , xK_period), sendMessage (IncMasterN (-1))) -- %! Deincrement the number of windows in the master area
+
+  -- quit, or restart
+  , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess)) -- %! Quit xmonad
+  , ((modMask              , xK_q     ), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi") -- %! Restart xmonad
+
+  , ((modMask .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -")) -- %! Run xmessage with a summary of the default keybindings (useful for beginners)
+  -- repeat the binding for non-American layout keyboards
+  , ((modMask              , xK_question), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
   ]
+  ++
+  -- mod-[1..9] %! Switch to workspace N
+  -- mod-shift-[1..9] %! Move client to workspace N
+  [((m .|. modMask, k), windows $ f i)
+    | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+    , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+  ++
+  -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
+  -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
+  [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+    | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 startup = do
   spawn "feh --bg-fill ~/Pictures/bg.jpg"
