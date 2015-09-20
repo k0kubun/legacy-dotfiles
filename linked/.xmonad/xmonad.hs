beb1a277471754a42fa0201f4a82ac891bd6ba12
Takashi Kokubun <takashikkbn@gmail.com>
2015-09-20 10:00:53 +0900

    Xmonad change by control

--- linked/.xmonad/xmonad.hs
+++ linked/.xmonad/xmonad.hs
@@ -92,7 +92,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   ++
   -- mod-[1..9] %! Switch to workspace N
   -- mod-shift-[1..9] %! Move client to workspace N
-  [((m .|. modMask, k), windows $ f i)
+  [((m .|. controlMask, k), windows $ f i)
     | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
     , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
   -- ++
