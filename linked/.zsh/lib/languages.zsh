fa496c69b17d1639f58fd4d39124e007fac0e712
Takashi Kokubun <takashikkbn@gmail.com>
2015-10-03 09:01:17 +0900

    Compile with gcc

--- linked/.zsh/lib/languages.zsh
+++ linked/.zsh/lib/languages.zsh
@@ -19,3 +19,8 @@ function tex() {
 # Haskell
 export PATH=$HOME/.cabal/bin:$PATH
 alias ce="cabal exec"
+
+# C
+export CC=/usr/local/opt/apple-gcc42/bin/gcc-4.2
+export CXX=/usr/local/opt/apple-gcc42/bin/g++-4.2
+export CPP=/usr/local/opt/apple-gcc42/bin/cpp-4.2
