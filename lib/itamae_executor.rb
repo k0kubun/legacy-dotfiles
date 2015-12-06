00e3be3bc7152ee9d9117965243a16e535782482
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-12-06 20:51:42 +0900

    Show itamae command in executor

--- lib/itamae_executor.rb
+++ lib/itamae_executor.rb
@@ -1,25 +1,35 @@
 module ItamaeExecutor
-  def self.role_name
-    case `uname`.strip
-    when 'Darwin'
-      'darwin'
-    else
-      abort "Unexpected uname: #{`uname`}"
+  class << self
+    def role_name
+      case `uname`.strip
+      when 'Darwin'
+        'darwin'
+      else
+        abort "Unexpected uname: #{`uname`}"
+      end
     end
-  end
 
-  def self.execute_role(role, itamae_options: nil, stackprof: false)
-    node_path = File.expand_path("../roles/#{role}/node.yml", __dir__)
-    cmd = %w[bundle exec itamae local]
-    cmd.push(*itamae_options.split(' ')) if itamae_options
-    cmd.push('-y', node_path) if File.exist?(node_path)
-    cmd << 'lib/stackprof_runner.rb' if stackprof
-    cmd << 'lib/recipe_helper.rb'
-    cmd << "roles/#{role}/default.rb"
-    system(*cmd)
-  end
+    def execute_role(role, itamae_options: nil, stackprof: false)
+      cmd = itamae_local(role, itamae_options: itamae_options, stackprof: stackprof)
+      cmd.push('lib/recipe_helper.rb', "roles/#{role}/default.rb")
+      printf "\e[32m#{cmd.join(' ')}\e[0m\n"
+      system(*cmd)
+    end
 
-  def self.execute(itamae_options: nil, stackprof: false)
-    execute_role(role_name, itamae_options: itamae_options, stackprof: stackprof)
+    def execute(itamae_options: nil, stackprof: false)
+      execute_role(role_name, itamae_options: itamae_options, stackprof: stackprof)
+    end
+
+    private
+
+    def itamae_local(role, itamae_options:, stackprof:)
+      node_path = File.expand_path("../roles/#{role}/node.yml", __dir__)
+
+      cmd = %w[bundle exec itamae local]
+      cmd.push(*itamae_options.split(' ')) if itamae_options
+      cmd.push('-y', node_path) if File.exist?(node_path)
+      cmd << 'lib/stackprof_runner.rb' if stackprof
+      cmd
+    end
   end
 end
