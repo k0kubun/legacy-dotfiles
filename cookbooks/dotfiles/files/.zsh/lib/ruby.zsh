c04c72d5bb054119676850869bde36d1ddf7a3a4
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-12-08 22:16:36 +0900

    Resurrect submodule

---
+++ cookbooks/dotfiles/files/.zsh/lib/ruby.zsh
@@ -0,0 +1,22 @@
+# rbenv
+export PATH="$HOME/.rbenv/bin:$PATH"
+
+# FIXME: bottle neck for sourcing .zshrc
+# eval "$(rbenv init - zsh)"
+eval "$(rbenv init - --no-rehash zsh)"
+
+# bundler
+alias be="bundle exec"
+
+# bundle open
+export BUNDLER_EDITOR="$(brew --prefix)/bin/vim"
+
+# rails
+alias -g RET="RAILS_ENV=test"
+alias -g RED="RAILS_ENV=development"
+alias -g REP="RAILS_ENV=production"
+
+# hash rocket
+function hr() {
+	sed -i '' -e 's/:\([a-zA-Z_]*\) =>/\1:/g' $1
+}
