bc255d593f88174e8f8dfe30fa2e7cd38634e5a6
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-12-29 21:36:18 +0900

    Fix itamae execution

--- itamae/cookbooks/dotfiles/default.rb
+++ itamae/cookbooks/dotfiles/default.rb
@@ -5,5 +5,6 @@ node.reverse_merge!(
 node[:dotfiles].each do |file|
   link File.join(node[:dotfiles_directory], file) do
     to File.expand_path(File.join('../../../', file), __dir__)
+    force true
   end
 end
