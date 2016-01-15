c823d12fd109cf0be0bb9c3ff99c8a0c80da44f9
Takashi Kokubun <takashikkbn@gmail.com>
2016-01-15 23:49:06 +0900

    Introduce X

--- roles/linux/default.rb
+++ roles/linux/default.rb
@@ -1,1 +1,11 @@
-# include_cookbook 'dotfiles'
+package 'xorg-server'
+package 'xorg-xinit'
+package 'xf86-video-intel'
+
+package 'xmonad'
+package 'xmonad-contrib'
+package 'dmenu'
+package 'xmobar'
+
+package 'xterm'
+package 'rxvt-unicode'
