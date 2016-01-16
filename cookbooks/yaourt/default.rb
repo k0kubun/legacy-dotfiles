e83af0a4bd9d0a3dd37156229716f0559c5d8e6b
Takashi Kokubun <takashikkbn@gmail.com>
2016-01-17 03:21:22 +0900

    Escape echo source too

--- cookbooks/yaourt/default.rb
+++ cookbooks/yaourt/default.rb
@@ -1,10 +1,12 @@
 yaourt_server = 'http://repo.archlinux.fr/$arch'
 yaourt_conf = <<-CONF
+
 [archlinuxfr]
 SigLevel = Never
 Server = #{yaourt_server}
+
 CONF
 
-execute "echo #{yaourt_conf.inspect} | sudo tee -a /etc/pacman.conf" do
+execute "echo #{Shellwords.escape(yaourt_conf)} | sudo tee -a /etc/pacman.conf" do
   not_if "grep -q #{Shellwords.escape(yaourt_server)} /etc/pacman.conf"
 end
