5faa11546c4c9a001696f4353a9a00a11ee18a96
Takashi Kokubun <takashikkbn@gmail.com>
2016-02-02 02:11:25 +0900

    patch is necessary to build mysql56

--- roles/linux/sudo.rb
+++ roles/linux/sudo.rb
@@ -32,8 +32,9 @@ package 'rxvt-unicode'
 # build deps
 package 'autoconf'
 package 'automake'
-package 'pkg-config'
 package 'fakeroot'
+package 'patch'
+package 'pkg-config'
 
 # extra
 package 'go'
