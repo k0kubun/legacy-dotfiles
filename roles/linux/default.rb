cb93fe43edb7c2ca7c7bb9efcaede6e122d38e05
Takashi Kokubun <takashikkbn@gmail.com>
2016-02-02 01:51:37 +0900

    Revert "Temporarily comment out"
    This reverts commit f93963668fec0e75505914d689b2cc4749b1e33b.

--- roles/linux/default.rb
+++ roles/linux/default.rb
@@ -5,35 +5,34 @@ define :yaourt do
   end
 end
 
-# include_recipe 'background'
+include_recipe 'background'
 
 # desktop apps
-# yaourt 'google-chrome'
+yaourt 'google-chrome'
 
 # input method
-# yaourt 'ibus'
-# yaourt 'ibus-qt'
-# yaourt 'ibus-skk'
-# yaourt 'skk-jisyo'
+yaourt 'ibus'
+yaourt 'ibus-qt'
+yaourt 'ibus-skk'
+yaourt 'skk-jisyo'
 
 # utils
-# yaourt 'dnsutils'
-# yaourt 'feh'
-# yaourt 'gtk-theme-arc'
-# yaourt 'htop'
-# yaourt 'time'
+yaourt 'dnsutils'
+yaourt 'feh'
+yaourt 'gtk-theme-arc'
+yaourt 'htop'
+yaourt 'time'
 yaourt 'tmux'
-# yaourt 'trayer'
-# yaourt 'unrar'
+yaourt 'trayer'
+yaourt 'unrar'
 yaourt 'vim'
 
 # fonts
 yaourt 'ttf-inconsolata'
-# yaourt 'ttf-migu'
-# yaourt 'adobe-source-sans-pro-fonts'
+yaourt 'ttf-migu'
+yaourt 'adobe-source-sans-pro-fonts'
 
 # development
-yaourt 'go'
-# yaourt 'mysql56'
-# yaourt 'nodejs'
-# yaourt 'npm'
+yaourt 'mysql56'
+yaourt 'nodejs'
+yaourt 'npm'
