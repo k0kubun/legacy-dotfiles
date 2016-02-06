2821a5899e3a2e230c2554992a3070a452e24dbe
Takashi Kokubun <takashikkbn@gmail.com>
2016-02-06 17:21:29 +0900

    Add packages required for development

--- roles/linux/pacman.rb
+++ roles/linux/pacman.rb
@@ -10,6 +10,7 @@ package 'make'
 # X window system
 package 'xorg-server'
 package 'xorg-server-utils'
+package 'xorg-server-xvfb'
 package 'xorg-xinit'
 package 'xorg-xmodmap'
 package 'xf86-video-intel'
