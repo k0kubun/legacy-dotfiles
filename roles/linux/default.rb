397901fd0f6065de9564acddf806b31e4c52b15b
Takashi Kokubun <takashikkbn@gmail.com>
2016-02-28 16:58:01 +0900

    Change to HiDPI
    I bought a 4K display

--- roles/linux/default.rb
+++ roles/linux/default.rb
@@ -3,3 +3,8 @@ include_recipe 'background'
 include_recipe 'xmonad_screenshot'
 include_cookbook 'tmux-conf'
 include_cookbook 'utils'
+
+execute 'HiDPI' do
+  command 'gsettings set org.gnome.desktop.interface scaling-factor 2'
+  not_if 'gsettings get org.gnome.desktop.interface scaling-factor | grep "^uint32 2$"'
+end
